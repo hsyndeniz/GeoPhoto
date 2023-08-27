ac793a4ce65026c43e04e186368ac13525e43185
hsyndeniz <huseyin.deniz1@hotmail.com>
2023-08-27 04:26:06 +0300

    update exif service

--- src/store/efix/index.ts
+++ src/store/efix/index.ts
@@ -3,28 +3,22 @@ import { Asset } from 'react-native-image-picker';
 
 const slice = createSlice({
   name: 'efix',
-  initialState: { image: null, metadata: null } as EfixState,
+  initialState: { image: null } as EfixState,
   reducers: {
     setImage: (state, { payload: { image } }: EfixPayload) => {
       if (typeof image !== 'undefined') {
         state.image = image;
       }
     },
-    setMetadata: (state, { payload: { metadata } }: EfixPayload) => {
-      if (typeof metadata !== 'undefined') {
-        state.metadata = metadata;
-      }
-    },
   },
 });
 
-export const { setImage, setMetadata } = slice.actions;
+export const { setImage } = slice.actions;
 
 export default slice.reducer;
 
 export type EfixState = {
   image: Asset | null;
-  metadata: any | null;
 };
 
 type EfixPayload = {
